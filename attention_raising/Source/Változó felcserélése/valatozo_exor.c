e7e8f37490d52d79fb39b314c5727181a214ea08
fupn26 <furjes.peter99@gmail.com>
2019-02-22 21:12:51 +0000

    Változó csere exorral

---
+++ attention_raising/Source/Változó felcserélése/valatozo_exor.c
@@ -0,0 +1,22 @@
+//segédváltozó cseréje exorral
+
+#include <stdio.h>
+int main()
+{
+	int a = 5;
+	int b = 7;
+	printf("Az a értéke:");
+	printf("%d\n",a);
+	printf("Az b értéke:");
+	printf("%d\n",b);
+	
+	//csere
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("Az a értéke:");
+	printf("%d\n",a);
+	printf("Az b értéke:");
+	printf("%d\n",b);
+	
+}
