49231506ae72c9047fd8167bea5f6ce8a6b1214f
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-03-12 08:47:11 +0100

    Forrás feltöltve

---
+++ attention_raising/Source/BNF/hiba.c
@@ -0,0 +1,7 @@
+//fordítsd: gcc hiba.c -o hiba -std=c89
+// majd: gcc hiba.c -o hiba -std = c99
+int main()
+                    {
+                        int a = 10; //C99-ben ezt szabad
+                        return 0;
+                    }
