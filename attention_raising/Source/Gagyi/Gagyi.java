240d4a9c43ba21b66067c82459016cdeccfba8c2
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-09-21 10:32:14 +0000

    Gagyi feladat kidolgozva, forrás feltöltve

---
+++ attention_raising/Source/Gagyi/Gagyi.java
@@ -0,0 +1,15 @@
+class Gagyi{
+	public static void main (String[] args){
+            if (args.length != 1){
+                System.out.println("Használat: java Gagyi szám");
+                return;
+            }
+            
+            Integer x = Integer.parseInt(args[0]);
+            Integer t = Integer.parseInt(args[0]);
+                
+            while (x >= t && x <= t && x != t){
+                System.out.println("Gagyiiii");
+            }
+	}
+} 
