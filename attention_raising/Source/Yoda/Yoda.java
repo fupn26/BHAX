430971c5ec0ca2ea3e106a5ddbe4883750f8c165
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-09-21 21:17:57 +0200

    Yoda conditions forrás

---
+++ attention_raising/Source/Yoda/Yoda.java
@@ -0,0 +1,10 @@
+class Yoda{
+    public static void main (String[] args){
+        String a = null;
+
+//        if(a.equals("Yoda")){
+        if("Yoda".equals(a)){
+            System.out.println("Yoda");
+        }
+    }
+}
