b7d302a560b59b7b191a78fbb3d561d3ef7f0611
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-10-13 16:40:58 +0200

    LeetCipher forrás hozzáadva

--- attention_raising/Source/LeetCipher/mainwindow.h
+++ attention_raising/Source/LeetCipher/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGridLayout>
+#include <QLabel>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QCheckBox>
+
+#include <cipher.h>
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +17,21 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    QGridLayout     *m_layout;
+    QWidget         *m_widget;
+    QTextEdit       *m_textedit_result;
+    QTextEdit       *m_textedit;
+    QPushButton     *m_pushbutton;
+    Cipher          *m_cipher;
+    QCheckBox       *m_checkbox;
+
+    bool            autotrans_on;
+
+public slots:
+    void get_encription();
+
+    void set_autotrans_on();
 };
 #endif // MAINWINDOW_H
