5d3b9dbc0dff5c24f2e9175bb03b54bd9c5f9f98
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-10-13 16:40:09 +0200

    LeetCipher forrás hozzáadva

--- attention_raising/Source/LeetCipher/cipher.h
+++ attention_raising/Source/LeetCipher/cipher.h
@@ -1,11 +1,26 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
+#include <QVector>
+#include <ctype.h>
+#include <QRandomGenerator>
 
 class Cipher
 {
 public:
     Cipher();
+    QString get_result();
+    void encryption(QString);
+
+private:
+    struct m_cipher{
+        char c;
+        char const *leet[4];
+    };
+
+    QVector<struct m_cipher> m_vector;
+
+    QString m_result;
 };
 
 #endif // CIPHER_H
