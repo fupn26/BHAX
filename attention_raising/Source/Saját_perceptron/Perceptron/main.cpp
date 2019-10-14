c01640061fe5d7d6a143f534d7ae3f562794cb5d
Fürjes-Benke Péter <fupn26@hotmail.hu>
2019-10-14 10:15:44 +0200

    Perceptron kidolgozva

---
+++ attention_raising/Source/Saját_perceptron/Perceptron/main.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+
+#include <iostream>
+#include "../perceptron.hpp"
+#include "png++/png.hpp"
+#include <time.h>
+
+int main (int argc, char **argv)
+{
+    png::image <png::rgb_pixel> png_image (argv[1]);
+
+    int size = png_image.get_width() * png_image.get_height();
+
+    Perceptron* p = new Perceptron (3, size, 256, 1);
+
+    double* image = new double[size];
+
+    for (int i = 0; i<png_image.get_width(); ++i)
+        for (int j = 0; j<png_image.get_height(); ++j)
+            image[i*png_image.get_width() + j] = png_image[i][j].red;
+
+
+    double* newPNG = new double[size];
+    srand(time(nullptr));
+
+    for(int i = 0; i < png_image.get_width(); ++i)
+        for(int j = 0; j<png_image.get_height(); ++j){
+            png_image[i][j].green = rand()%256;//newPNG[j*png_image.get_height()+j];
+            png_image[i][j].red = rand()%256;
+            png_image[i][j].blue = rand() % 256;
+        }
+    png_image.write("output.png");
+
+
+    double value = (*p) (image);
+
+    std::cout << value << std::endl;
+
+    delete p;
+    delete [] image;
+
+}
